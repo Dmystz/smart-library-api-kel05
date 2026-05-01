8fe035ea776f5824bfe525381afa2e2e24736b2f
Dmystz <idimasprayogo123@gmail.com>
2026-05-01 15:19:47 +0700

    update

--- src/models/memberModel.js
+++ src/models/memberModel.js
@@ -8,14 +8,14 @@ export const getById = (id) =>
 
 export const create = (data) =>
   pool.query(
-    "INSERT INTO members(name,email) VALUES($1,$2) RETURNING *",
-    [data.name, data.email]
+    "INSERT INTO members(full_name,email,member_type) VALUES($1,$2,$3) RETURNING *",
+    [data.full_name, data.email, data.member_type]
   );
 
 export const update = (id, data) =>
   pool.query(
-    "UPDATE members SET name=$1, email=$2 WHERE id=$3 RETURNING *",
-    [data.name, data.email, id]
+    "UPDATE members SET full_name=$1, email=$2, member_type=$3 WHERE id=$4 RETURNING *",
+    [data.full_name, data.email, data.member_type, id]
   );
 
 export const deleteMember = (id) =>
